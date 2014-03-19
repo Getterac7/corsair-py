cc8bb40d6be37ad6acf62e9e985c137c3c8b2b60
wally <wizkid8586@gmail.com>
2014-03-19 15:56:04 -0400

    Used a more robust launcher.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from os.path import join
-import sys
+import os, sys, inspect
 
-sys.path = [join(sys.path[0],"data")] + sys.path
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"data")))
+if cmd_subfolder not in sys.path:
+	sys.path.insert(0, cmd_subfolder)
 
 import game
 
